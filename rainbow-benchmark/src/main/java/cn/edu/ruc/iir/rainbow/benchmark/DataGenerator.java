8cdabdc6c85d81553b3979c1d74afd3b9f4a9f27
taoyouxian <1730853800@qq.com>
2017-07-27 16:35:58 +0800

    20170727 Second Commit by TaoYou xian

--- rainbow-benchmark/src/main/java/cn/edu/ruc/iir/rainbow/benchmark/DataGenerator.java
+++ rainbow-benchmark/src/main/java/cn/edu/ruc/iir/rainbow/benchmark/DataGenerator.java
@@ -1,15 +1,127 @@
 package cn.edu.ruc.iir.rainbow.benchmark;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @version V1.0
  * @Package: cn.edu.ruc.iir.rainbow.benchmark
  * @ClassName: DataGenerator
  * @Description: To generate data for test
  * @author: Tao
- * @date: Create in 2017-07-27 12:00
+ * @date: Create in 2017-07-27 14:20
  **/
 public class DataGenerator {
 
+    public String schema_origin = "data/schema.txt";
+    public String workload = "data/workload.txt";
+    public String filePath = null;
 
+    // column mapping
+    public Map<String, String> columnMap = new HashMap<String, String>();
+
+    private static DataGenerator instance = null;
+
+    public static DataGenerator Instance() {
+        if (instance == null) {
+            instance = new DataGenerator();
+        }
+        return instance;
+    }
+
+    /**
+     * @ClassName: DataGenerator
+     * @Title:
+     * @Description: Change Column, make column_mapping.csv & schema_new.txt
+     * @param:
+     * @date: 16:24 2017/7/27
+     */
+    public void setColumnShift() {
+        filePath = this.getClass().getClassLoader()
+                .getResource((schema_origin)).getFile();
+        String curLine = null;
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        BufferedWriter bw1 = null;
+        String outCsvPath = filePath.replace(schema_origin, "column_mapping.csv");
+        String outSchemaPath = filePath.replace(schema_origin, "schema_new.txt");
+        int i = 1;
+        String newColumnName = null;
+        String mapLine[] = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            bw = new BufferedWriter(new FileWriter(outCsvPath));
+            bw1 = new BufferedWriter(new FileWriter(outSchemaPath));
+            while ((curLine = br.readLine()) != null) {
+                mapLine = curLine.split("\t");
+                newColumnName = "Column_" + i++;
+                bw.write(mapLine[0] + "," + newColumnName + "\n");
+                bw1.write(newColumnName + "\t" + mapLine[1] + "\t" + mapLine[2] + "\n");
+                columnMap.put(mapLine[0], newColumnName);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null || bw != null || bw1 != null)
+                try {
+                    br.close();
+                    bw.close();
+                    bw1.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    /**
+     * @ClassName: DataGenerator
+     * @Title:
+     * @Description: make workload_new.csv
+     * @param:
+     * @date: 16:28 2017/7/27
+     */
+    public void setWorkloadShift() {
+        filePath = this.getClass().getClassLoader()
+                .getResource((workload)).getFile();
+        String curLine = null;
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        String outWorkloadPath = filePath.replace(workload, "workload_new.txt");
+        String splitLine[] = null;
+        String columnsLine[] = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            bw = new BufferedWriter(new FileWriter(outWorkloadPath));
+            while ((curLine = br.readLine()) != null) {
+                splitLine = curLine.split("\t");
+                // set column1 only
+                // curLine = curLine.replace(splitLine[0], UUID.randomUUID().toString());
+                columnsLine = splitLine[2].split(",");
+                for (int k = 0; k < columnsLine.length; k++) {
+                    if (columnMap.get(columnsLine[k]) != null) {
+                        curLine = curLine.replace(columnsLine[k], columnMap.get(columnsLine[k]));
+                    } else {
+                        System.out.println(columnsLine[k] + " do not have a mapping");
+                    }
+                }
+                bw.write(curLine + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null || bw != null)
+                try {
+                    br.close();
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
 
 }
---
+++ rainbow-benchmark/src/test/java/cn/edu/ruc/iir/rainbow/benchmark/TestDataGenerator.java
@@ -0,0 +1,34 @@
+package cn.edu.ruc.iir.rainbow.benchmark;
+
+import org.junit.Test;
+
+/**
+ * @version V1.0
+ * @Package: cn.edu.ruc.iir.rainbow.benchmark
+ * @ClassName: TestDataGenerator
+ * @Description: To Test functions of Class DataGenerator
+ * @author: Tao
+ * @date: Create in 2017-07-27 15:02
+ **/
+public class TestDataGenerator {
+
+    private DataGenerator dataGenerator = DataGenerator.Instance();
+
+    @Test
+    public void TestSetColumnShift() {
+        dataGenerator.setColumnShift();
+    }
+
+    @Test
+    public void TestFilePath() {
+        String path = this.getClass().getClassLoader()
+                .getResource((dataGenerator.schema_origin)).getFile();
+        System.out.println("path is : " + path);
+    }
+
+    @Test
+    public void TestSetWorkloadShift() {
+        dataGenerator.setColumnShift();
+        dataGenerator.setWorkloadShift();
+    }
+}
