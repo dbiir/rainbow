6b316429f5ecc7fcc7af2cf1c2d08b7913c19d88
taoyouxian <1730853800@qq.com>
2017-07-27 12:03:07 +0800

    First commit by TaoYou xian

---
+++ rainbow-benchmark/src/main/java/cn/edu/ruc/iir/rainbow/benchmark/DataGenerator.java
@@ -0,0 +1,15 @@
+package cn.edu.ruc.iir.rainbow.benchmark;
+
+/**
+ * @version V1.0
+ * @Package: cn.edu.ruc.iir.rainbow.benchmark
+ * @ClassName: DataGenerator
+ * @Description: To generate data for test
+ * @author: Tao
+ * @date: Create in 2017-07-27 12:00
+ **/
+public class DataGenerator {
+
+
+
+}
