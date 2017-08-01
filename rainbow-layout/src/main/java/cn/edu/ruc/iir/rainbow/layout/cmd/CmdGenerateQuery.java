24eb8308f5ed40f7ed52bbd4a0b0bbbf21bd3560
bianhq <bianhaoqiong@gmail.com>
2017-08-01 19:40:59 +0800

    fix issue #7

--- rainbow-layout/src/main/java/cn/edu/ruc/iir/rainbow/layout/cmd/CmdGenerateQuery.java
+++ rainbow-layout/src/main/java/cn/edu/ruc/iir/rainbow/layout/cmd/CmdGenerateQuery.java
@@ -2,7 +2,12 @@ package cn.edu.ruc.iir.rainbow.layout.cmd;
 
 import cn.edu.ruc.iir.rainbow.common.cmd.Command;
 import cn.edu.ruc.iir.rainbow.common.cmd.Receiver;
+import cn.edu.ruc.iir.rainbow.common.exception.ColumnNotFoundException;
+import cn.edu.ruc.iir.rainbow.common.exception.ExceptionHandler;
+import cn.edu.ruc.iir.rainbow.common.exception.ExceptionType;
+import cn.edu.ruc.iir.rainbow.layout.sql.GenerateQuery;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class CmdGenerateQuery implements Command
@@ -18,50 +23,46 @@ public class CmdGenerateQuery implements Command
     /**
      * params should contain the following settings:
      * <ol>
-     *   <li>table.type, ordered, dupped, origin</li>
-     *   <li>file.format, orc, parquet, text</li>
+     *   <li>table.name</li>
+     *   <li>ordered.table.name</li>
+     *   <li>hostname</li>
      *   <li>schema.file</li>
-     *   <li>ddl.file</li>
+     *   <li>workload.file</li>
+     *   <li>spark.query.file</li>
+     *   <li>hive.query.file</li>
      * </ol>
      *
      * this method will pass the following results to receiver:
      * <ol>
-     *   <li>ddl.file</li>
+     *   <li>spark.query.file</li>
+     *   <li>hive.query.file</li>
      * </ol>
      * @param params
      */
     @Override
     public void execute(Properties params)
     {
-        String algoName = params.getProperty("algorithm.name");
-        String schemaFilePath = params.getProperty("schema.file.path");
-        String workloadFilePath = params.getProperty("workload.file.path");
-        String duppedSchemaFilePath = params.getProperty("dupped.schema.file.path");
-        String duppedWorkloadFilePath = params.getProperty("dupped.workload.file.path");
+        String tableName = params.getProperty("table.name");
+        String orderedTableName = params.getProperty("ordered.table.name");
+        String hostname = params.getProperty("hostname");
+        String schemaFilePath = params.getProperty("schema.file");
+        String workloadFilePath = params.getProperty("workload.file");
+        String sparkQueryFilePath = params.getProperty("spark.query.file");
+        String hiveQueryFilePath = params.getProperty("hive.query.file");
 
         Properties results = new Properties();
-        //try
-        //{
-
-        results.setProperty("dupped.schema.file.path", duppedSchemaFilePath);
-        results.setProperty("dupped.workload.file.path", duppedWorkloadFilePath);
-        //}
-        /* catch (IOException e)
+        try
+        {
+            GenerateQuery.Gen(tableName, orderedTableName, hostname, schemaFilePath, workloadFilePath, sparkQueryFilePath, hiveQueryFilePath);
+            results.setProperty("spark.query.file", sparkQueryFilePath);
+            results.setProperty("hive.query.file", hiveQueryFilePath);
+        } catch (IOException e)
         {
             ExceptionHandler.Instance().log(ExceptionType.ERROR, "I/O error, check the file paths", e);
-        } catch (InterruptedException e)
-        {
-            ExceptionHandler.Instance().log(ExceptionType.ERROR, "interrupted while waiting for algorithm execution", e);
         } catch (ColumnNotFoundException e)
         {
-            ExceptionHandler.Instance().log(ExceptionType.ERROR, "column not fount when building workload", e);
-        } catch (ClassNotFoundException e)
-        {
-            ExceptionHandler.Instance().log(ExceptionType.ERROR, "algorithm class not fount", e);
-        } catch (AlgoException e)
-        {
-            ExceptionHandler.Instance().log(ExceptionType.ERROR, "algorithm initialization error", e);
-        }*/
+            ExceptionHandler.Instance().log(ExceptionType.ERROR, "column not fount when generating queries", e);
+        }
 
         if (this.receiver == null)
         {
